0bac3f2324545bea440a44bcf102a3219ef7e04a
dyotamo <dyotamo@gmail.com>
2020-03-26 10:00:46 +0200

    Changing swatch color.

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.teal),
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: HomeScreen(),
     );
   }
